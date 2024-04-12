b2fe877e5e54c1ed9cf91e14098d2355b71f0510
GlebSolovev <lionyoung@mail.ru>
2024-04-12 06:03:19 +0200

    Design & implement new benchmarking api
    Support switching workspaces, traversing directories and proving theorems

---
+++ src/test/benchmark/runBenchmark.test.ts
@@ -0,0 +1,127 @@
+import * as fs from "fs";
+import * as path from "path";
+
+import { UserModelsParams } from "../../llm/userModelParams";
+
+import { BenchmarkResult, runTestBenchmark } from "./benchmarkingFramework";
+import { code, consoleLogLine } from "./loggingUtils";
+import { onlyAutoModelsParams } from "./presets";
+
+interface Benchmark {
+    name: string;
+    items: DatasetItem[];
+    modelsParams: UserModelsParams;
+    requireAllAdmitsCompleted: Boolean;
+    timeoutMinutes: number;
+}
+
+class DatasetItem {
+    workspaceRootPath: string | undefined;
+
+    /* Paths should be relative to 'dataset' folder */
+    constructor(
+        public path: string,
+        workspaceRootPath: string | undefined = undefined
+    ) {
+        this.workspaceRootPath = workspaceRootPath;
+    }
+}
+
+const simpleAutoBenchmark: Benchmark = {
+    name: "Complete simple examples with `auto`",
+    items: [new DatasetItem("auto_benchmark.v")],
+    modelsParams: onlyAutoModelsParams,
+    requireAllAdmitsCompleted: true,
+    timeoutMinutes: 1,
+};
+
+const mixedAutoBenchmark: Benchmark = {
+    name: "Complete mixed examples (both simple & hard) with `auto`",
+    items: [new DatasetItem("mixed_benchmark.v")],
+    modelsParams: onlyAutoModelsParams,
+    requireAllAdmitsCompleted: false,
+    timeoutMinutes: 1,
+};
+
+const benchmarks: Benchmark[] = [simpleAutoBenchmark, mixedAutoBenchmark];
+
+suite("Benchmark", () => {
+    const benchmarkDir = getBenchmarkDir();
+    const datasetDir = path.join(benchmarkDir, "dataset");
+
+    for (const benchmark of benchmarks) {
+        test(benchmark.name, async () => {
+            const admitsCompletedInTotal = new BenchmarkResult(0, 0);
+            const theoremsProvedInTotal = new BenchmarkResult(0, 0);
+
+            for (const item of benchmark.items) {
+                const resolvedWorkspaceRootPath = item.workspaceRootPath
+                    ? path.join(datasetDir, item.workspaceRootPath)
+                    : undefined;
+
+                const resolvedItemPath = path.join(datasetDir, item.path);
+                const itemPathStats = getPathStats(resolvedItemPath);
+                if (!itemPathStats.isDirectory() && !itemPathStats.isFile()) {
+                    throw Error(`unsupported path type: ${item.path}`);
+                }
+                const resolvedFilePaths = itemPathStats.isDirectory()
+                    ? findSourceFiles(resolvedItemPath)
+                    : [resolvedItemPath];
+
+                for (const resolvedFilePath of resolvedFilePaths) {
+                    const { admitsCompleted, theoremsProved } =
+                        await runTestBenchmark(
+                            resolvedFilePath,
+                            benchmark.modelsParams,
+                            resolvedWorkspaceRootPath,
+                            benchmark.requireAllAdmitsCompleted
+                        );
+                    admitsCompletedInTotal.add(admitsCompleted);
+                    theoremsProvedInTotal.add(theoremsProved);
+                }
+            }
+
+            consoleLogLine();
+            consoleLogLine("\n");
+            console.log(
+                `${code("magenta")}BENCHMARK REPORT:${code("reset")} ${benchmark.name}`
+            );
+            console.log(
+                `- ADMITS COMPLETED IN TOTAL: ${admitsCompletedInTotal}`
+            );
+            console.log(
+                `- THEOREMS PROVED IN TOTAL: ${theoremsProvedInTotal}\n`
+            );
+        }).timeout(benchmark.timeoutMinutes * 60 * 1000);
+    }
+});
+
+function getBenchmarkDir(): string {
+    const dirname: string = path.join(__dirname, "../../../");
+    return path.join(dirname, "src", "test", "benchmark");
+}
+
+function getPathStats(path: string): fs.Stats {
+    return fs.lstatSync(path);
+}
+
+function findSourceFiles(directoryPath: string): string[] {
+    let sourceFilePaths: string[] = [];
+    function traverseDirectory(curDirectoryPath: string) {
+        fs.readdirSync(curDirectoryPath).forEach((child) => {
+            const childPath = path.join(curDirectoryPath, child);
+            if (fs.statSync(childPath).isDirectory()) {
+                traverseDirectory(childPath);
+            } else {
+                if (
+                    path.extname(childPath) === ".v" &&
+                    !childPath.endsWith("_cp_aux.v")
+                ) {
+                    sourceFilePaths.push(childPath);
+                }
+            }
+        });
+    }
+    traverseDirectory(directoryPath);
+    return sourceFilePaths;
+}
