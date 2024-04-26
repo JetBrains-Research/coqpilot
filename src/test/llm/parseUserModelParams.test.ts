6b159bf320e67219493249a2460ef5c04c2de69c
GlebSolovev <lionyoung@mail.ru>
2024-04-27 01:08:19 +0200

    Test parsing of UserModelParams from JSON

---
+++ src/test/llm/parseUserModelParams.test.ts
@@ -0,0 +1,144 @@
+import Ajv, { JSONSchemaType } from "ajv";
+import { expect } from "earl";
+
+import {
+    grazieUserModelParamsSchema,
+    lmStudioUserModelParamsSchema,
+    openAiUserModelParamsSchema,
+    predefinedProofsUserModelParamsSchema,
+    userModelParamsSchema,
+    userMultiroundProfileSchema,
+} from "../../llm/userModelParams";
+
+suite("Parse UserModelParams from JSON test", () => {
+    const jsonSchemaValidator = new Ajv();
+
+    function validateJSON<T>(
+        json: any,
+        targetClassSchema: JSONSchemaType<T>,
+        expectedToBeValidJSON: boolean
+    ) {
+        const validate = jsonSchemaValidator.compile(targetClassSchema);
+        expect(validate(json as T)).toEqual(expectedToBeValidJSON);
+    }
+
+    function isValidJSON<T>(json: any, targetClassSchema: JSONSchemaType<T>) {
+        return validateJSON(json, targetClassSchema, true);
+    }
+
+    function isInvalidJSON<T>(json: any, targetClassSchema: JSONSchemaType<T>) {
+        return validateJSON(json, targetClassSchema, false);
+    }
+
+    const validMultiroundProfileComplete = {
+        maxRoundsNumber: 5,
+        proofFixChoices: 1,
+        proofFixPrompt: "fix me",
+    };
+
+    const validUserModelParamsCompelete = {
+        modelName: "model name",
+        choices: 30,
+        systemPrompt: "generate proof",
+        newMessageMaxTokens: 100,
+        tokensLimit: 2000,
+        multiroundProfile: validMultiroundProfileComplete,
+    };
+
+    const validOpenAiUserModelParamsComplete = {
+        ...validUserModelParamsCompelete,
+        temperature: 36.6,
+        apiKey: "api-key",
+    };
+    const validGrazieUserModelParamsComplete = {
+        ...validUserModelParamsCompelete,
+        apiKey: "api-key",
+    };
+    const validPredefinedProofsUserModelParamsComplete = {
+        ...validUserModelParamsCompelete,
+        tactics: ["auto.", "auto. intro."],
+    };
+    const validLMStudioUserModelParamsComplete = {
+        ...validUserModelParamsCompelete,
+        temperature: 36.6,
+        port: 555,
+    };
+
+    test("Validate `UserMultiroundProfile`", () => {
+        isValidJSON(
+            validMultiroundProfileComplete,
+            userMultiroundProfileSchema
+        );
+        const validUndefinedProp = {
+            maxRoundsNumber: 5,
+            proofFixChoices: 1,
+            proofFixPrompt: undefined,
+        };
+        isValidJSON(validUndefinedProp, userMultiroundProfileSchema);
+        const validEmpty = {};
+        isValidJSON(validEmpty, userMultiroundProfileSchema);
+
+        const invalidWrongTypeProp = {
+            ...validMultiroundProfileComplete,
+            proofFixPrompt: 0,
+        };
+        isInvalidJSON(invalidWrongTypeProp, userMultiroundProfileSchema);
+        const invalidAdditionalProp = {
+            ...validMultiroundProfileComplete,
+            something: "something",
+        };
+        isInvalidJSON(invalidAdditionalProp, userMultiroundProfileSchema);
+    });
+
+    test("Validate `UserModelParams`", () => {
+        isValidJSON(validUserModelParamsCompelete, userModelParamsSchema);
+        const validOnlyModelName = {
+            modelName: "the only model",
+        };
+        isValidJSON(validOnlyModelName, userModelParamsSchema);
+
+        const invalidNoModelName = {
+            choices: 30,
+            systemPrompt: "let's generate",
+        };
+        isInvalidJSON(invalidNoModelName, userModelParamsSchema);
+        const invalidWrongTypeProp = {
+            ...validMultiroundProfileComplete,
+            tokensLimit: "no limits",
+        };
+        isInvalidJSON(invalidWrongTypeProp, userMultiroundProfileSchema);
+        const invalidAdditionalProp = {
+            ...validUserModelParamsCompelete,
+            something: "something",
+        };
+        isInvalidJSON(invalidAdditionalProp, userMultiroundProfileSchema);
+    });
+
+    test("Validate `OpenAiUserModelParams`", () => {
+        isValidJSON(
+            validOpenAiUserModelParamsComplete,
+            openAiUserModelParamsSchema
+        );
+    });
+
+    test("Validate `GrazieUserModelParams`", () => {
+        isValidJSON(
+            validGrazieUserModelParamsComplete,
+            grazieUserModelParamsSchema
+        );
+    });
+
+    test("Validate `PredefinedProofsUserModelParams`", () => {
+        isValidJSON(
+            validPredefinedProofsUserModelParamsComplete,
+            predefinedProofsUserModelParamsSchema
+        );
+    });
+
+    test("Validate `LMStudioUserModelParams`", () => {
+        isValidJSON(
+            validLMStudioUserModelParamsComplete,
+            lmStudioUserModelParamsSchema
+        );
+    });
+});
