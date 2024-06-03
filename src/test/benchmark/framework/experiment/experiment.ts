ac6ae5787154a09b09dfff829d4094ecd060070f
GlebSolovev <lionyoung@mail.ru>
2024-06-03 10:41:34 +0200

    Implement draft benchmarking items build

--- src/test/benchmark/framework/experiment/experiment.ts
+++ src/test/benchmark/framework/experiment/experiment.ts
@@ -1,12 +1,13 @@
 import { benchmark } from "../benchmark";
 import { SeverityLevel } from "../logging/benchmarkingLogger";
 import { BenchmarkingItem } from "../structures/benchmarkingItem";
+import { TargetType } from "../structures/completionGenerationTask";
 import { ExperimentResults } from "../structures/experimentResults";
 import { LLMServiceIdentifier } from "../structures/llmServiceIdentifier";
 import { getRootDir, joinPaths, resolveAsAbsolutePath } from "../utils/fsUtils";
 
 import { InputBenchmarkingModelParams } from "./inputBenchmarkingModelParams";
-import { InputTargets } from "./targetsBuilder";
+import { InputTargets, mergeInputTargets } from "./targetsBuilder";
 
 export class Experiment {
     constructor(
@@ -26,6 +27,7 @@ export class Experiment {
         artifactsDirPath: string,
         loggerSeverity: SeverityLevel
     ): Promise<ExperimentResults> {
+        // TODO: build nix projects in dataset dir
         return benchmark(
             this.buildBenchmarkingItems(),
             resolveAsAbsolutePath(joinPaths(getRootDir(), artifactsDirPath)),
@@ -33,9 +35,60 @@ export class Experiment {
         );
     }
 
+    // TODO: add logging
     private buildBenchmarkingItems(): BenchmarkingItem[] {
-        // TODO: implement
-        return [];
+        const experimentInputTargets = mergeInputTargets(
+            this.bundles.map((bundle) => bundle.targets)
+        );
+        const benchmarkingItems: BenchmarkingItem[] = [];
+        for (const bundle of this.bundles) {
+            for (const [
+                workspaceRoot,
+                filePathToFileTargets,
+            ] of bundle.targets.entries()) {
+                // TODO: enter workspace
+                for (const [
+                    filePath,
+                    fileTarget,
+                ] of filePathToFileTargets.entries()) {
+                    // TODO: parse source file
+                    const targetTypesForAllTheorems = [];
+                    if (fileTarget.allTheoremsAsAdmitTargets) {
+                        targetTypesForAllTheorems.push(TargetType.ADMIT);
+                    }
+                    if (fileTarget.allTheoremsAsProveTheoremTargets) {
+                        targetTypesForAllTheorems.push(
+                            TargetType.PROVE_THEOREM
+                        );
+                    }
+                    targetTypesForAllTheorems.forEach(
+                        (targetType) => benchmarkingItems.push({}) // TODO: fill with data
+                    );
+                    for (const [
+                        theoremName,
+                        theoremTarget,
+                    ] of fileTarget.specificTheoremTargets) {
+                        const targetTypes = [];
+                        if (
+                            theoremTarget.admitTargets &&
+                            !fileTarget.allTheoremsAsAdmitTargets
+                        ) {
+                            targetTypes.push(TargetType.ADMIT);
+                        }
+                        if (
+                            theoremTarget.proveTheoremTarget &&
+                            !fileTarget.allTheoremsAsProveTheoremTargets
+                        ) {
+                            targetTypes.push(TargetType.PROVE_THEOREM);
+                        }
+                        targetTypes.forEach(
+                            (targetType) => benchmarkingItems.push({}) // TODO: fill with data
+                        );
+                    }
+                }
+            }
+        }
+        return benchmarkingItems;
     }
 }
 
