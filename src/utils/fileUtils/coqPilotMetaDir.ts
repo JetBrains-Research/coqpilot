c99c29064cdf884e3eb564d738975d28abd87004
Gleb Solovev <lionyoung@mail.ru>
2025-03-24 02:43:37 +0100

    Introduce `.coqpilot` hidden directory

---
+++ src/utils/fileUtils/coqPilotMetaDir.ts
@@ -0,0 +1,22 @@
+import { createDirectory, joinPaths } from "../fs";
+
+export const COQPILOT_META_DIR_NAME = ".coqpilot";
+export const COQPILOT_META_LOGS_SUBDIR_NAME = "logs";
+
+export function getCoqPilotMetaDirPath(projectRootPath: string): string {
+    return joinPaths(projectRootPath, COQPILOT_META_DIR_NAME);
+}
+
+export function getOrCreateCoqPilotMetaDir(projectRootPath: string): string {
+    return createDirectory(false, projectRootPath, COQPILOT_META_DIR_NAME);
+}
+
+export function getOrCreateCoqPilotMetaLogsDir(
+    projectRootPath: string
+): string {
+    return createDirectory(
+        false,
+        getCoqPilotMetaDirPath(projectRootPath),
+        COQPILOT_META_LOGS_SUBDIR_NAME
+    );
+}
