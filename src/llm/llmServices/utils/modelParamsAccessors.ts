3bbc648a6221313b82f1eda5ab8f5678e33685b3
Gleb Solovev <lionyoung@mail.ru>
2025-05-04 03:25:29 +0200

    Rename `modelName` getter

--- src/llm/llmServices/utils/modelParamsAccessors.ts
+++ src/llm/llmServices/utils/modelParamsAccessors.ts
@@ -1,5 +1,5 @@
 import { ModelParams } from "../modelParams";
 
-export function modelName(params: ModelParams): string | undefined {
+export function getModelName(params: ModelParams): string | undefined {
     return "modelName" in params ? (params.modelName as string) : "";
 }
