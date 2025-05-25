afafa9dcbdfff07e0d5c76b8c93b10cc15b201e3
Gleb Solovev <lionyoung@mail.ru>
2025-05-25 22:45:57 +0200

    Make UI tests conditional, disable at CI

--- src/test/extension/coqPilot.test.ts
+++ src/test/extension/coqPilot.test.ts
@@ -3,63 +3,76 @@ import { ConfigurationTarget, Uri, commands, workspace } from "vscode";
 import { PLUGIN_ID } from "../../extension/utils/pluginId";
 import { delay } from "../../utils/async/delay";
 import { time, timeToMillis } from "../../utils/time";
+import { testIf } from "../commonTestFunctions/conditionalTest";
 import { resolveResourcesDir } from "../commonTestFunctions/pathsResolver";
+import {
+    UI_TESTS_DISABLED_CAUSE,
+    checkUITestsEnabled,
+} from "../commonTestFunctions/uiTestsUtils";
 
 import { TextDocumentWrapper } from "./documentUtils";
 
-suite("Test CoqPilot Extension", () => {
-    test("Smoke test: prove project theorem with `auto.`", async () => {
-        const [filePath, workspaceRootPath] = resolveResourcesDir(
-            ["coqProj", "theories", "C.v"],
-            ["coqProj"]
-        );
-        const interactionDelayMillis = 1_000;
+suite("Test CoqPilot Extension", function () {
+    testIf(
+        checkUITestsEnabled(),
+        UI_TESTS_DISABLED_CAUSE,
+        this.title,
+        "Smoke test: prove project theorem with `auto.`",
+        async () => {
+            const [filePath, workspaceRootPath] = resolveResourcesDir(
+                ["coqProj", "theories", "C.v"],
+                ["coqProj"]
+            );
+            const interactionDelayMillis = 1_000;
 
-        workspace.updateWorkspaceFolders(
-            0,
-            workspace.workspaceFolders?.length ?? 0,
-            { uri: Uri.file(workspaceRootPath) }
-        );
-        await delay(interactionDelayMillis);
-        try {
-            const targetDoc = await TextDocumentWrapper.create(filePath);
+            workspace.updateWorkspaceFolders(
+                0,
+                workspace.workspaceFolders?.length ?? 0,
+                { uri: Uri.file(workspaceRootPath) }
+            );
             await delay(interactionDelayMillis);
+            try {
+                const targetDoc = await TextDocumentWrapper.create(filePath);
+                await delay(interactionDelayMillis);
 
-            targetDoc.checkDocumentContains("admit.");
+                targetDoc.checkDocumentContains("admit.");
 
-            await workspace
-                .getConfiguration("coqpilot")
-                .update(
-                    "openAiModelsParameters",
-                    [],
+                await workspace
+                    .getConfiguration("coqpilot")
+                    .update(
+                        "openAiModelsParameters",
+                        [],
+                        ConfigurationTarget.Workspace
+                    );
+                await workspace.getConfiguration("coqpilot").update(
+                    "predefinedProofsModelsParameters",
+                    [
+                        {
+                            modelId: "predefined-auto",
+                            tactics: ["auto."],
+                        },
+                    ],
                     ConfigurationTarget.Workspace
                 );
-            await workspace.getConfiguration("coqpilot").update(
-                "predefinedProofsModelsParameters",
-                [
-                    {
-                        modelId: "predefined-auto",
-                        tactics: ["auto."],
-                    },
-                ],
-                ConfigurationTarget.Workspace
-            );
-            await delay(interactionDelayMillis);
+                await delay(interactionDelayMillis);
 
-            await commands.executeCommand(
-                `${PLUGIN_ID}.perform_completion_for_all_admits`
-            );
-            await delay(interactionDelayMillis);
+                await commands.executeCommand(
+                    `${PLUGIN_ID}.perform_completion_for_all_admits`
+                );
+                await delay(interactionDelayMillis);
 
-            targetDoc.checkDocumentContains("auto.");
+                targetDoc.checkDocumentContains("auto.");
 
-            await commands.executeCommand("undo");
-            await commands.executeCommand("undo");
-            await delay(interactionDelayMillis);
+                await commands.executeCommand("undo");
+                await commands.executeCommand("undo");
+                await delay(interactionDelayMillis);
 
-            targetDoc.checkDocumentContains("admit.");
-        } finally {
-            await commands.executeCommand("workbench.action.closeAllEditors");
+                targetDoc.checkDocumentContains("admit.");
+            } finally {
+                await commands.executeCommand(
+                    "workbench.action.closeAllEditors"
+                );
+            }
         }
-    }).timeout(timeToMillis(time(30, "second")));
+    )?.timeout(timeToMillis(time(30, "second")));
 });
